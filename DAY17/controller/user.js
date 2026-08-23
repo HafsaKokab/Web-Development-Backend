33498f05f11eeedca96ffb7b599736cb87cd9083
HafsaKokab <hafsakokab01@gmail.com>
2026-08-23 16:27:03 +0500

    Add DAY17 backend work

---
+++ DAY17/controller/user.js
@@ -0,0 +1,40 @@
+const User = require("../models/user");
+const { setUser } = require("../service/auth");
+
+async function handleUserSignup(req, res) {
+    const { name, email, password } = req.body;
+
+    await User.create({
+        name,
+        email,
+        password,
+    });
+
+    return res.redirect("/");
+}
+
+async function handleUserLogin(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email, password });
+
+    if (!user) {
+        return res.render("login", {
+            error: "Invalid Username or Password",
+        });
+    }
+
+    console.log("LOGIN USER:", user);
+    console.log("LOGIN ROLE:", user.role);
+
+    const token = setUser(user);
+
+    res.cookie("token", token);
+
+    return res.redirect("/");
+}
+
+module.exports = {
+    handleUserSignup,
+    handleUserLogin,
+};
